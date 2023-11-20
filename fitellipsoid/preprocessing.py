888a03b70ec162e792d34ba3143dd0ca04a23767
Nguyen Trung <trung.nguyen@vtt.fi>
2023-11-20 19:37:16 +0200

    Update preprocessing script

--- fitellipsoid/preprocessing.py
+++ fitellipsoid/preprocessing.py
@@ -6,4 +6,34 @@ def grains(data):
         :param data: data path
         :return: 3 dictionaries of RVE coordinates, voxels' vertices and voxels' phases
     """
-    return data
+    ## Dictionaries to return
+    RVE      = {}
+    vertices = {}
+    phases   = {}
+
+    file_open = open(data, 'r')
+    ## Fill in the RVE dictionaries
+    for line in file_open:
+        line_split = line.split(" ")
+        grainID = int(line_split[-2])
+        if grainID in RVE:
+            RVE[grainID].append([int(line_split[3]), int(line_split[4]), int(line_split[5]), int(line_split[-1])])
+        else:
+            RVE[grainID] = [[int(line_split[3]), int(line_split[4]), int(line_split[5]), int(line_split[-1])]]
+    
+    ## Fill in the vertices & phases dictionaries
+    for i in RVE:
+        x, y, z, phase = np.transpose(RVE[i])
+        u = np.transpose([x, y, z])
+        u = np.append(u, np.transpose([x, y, z]) - [1, 0, 0], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [0, 1, 0], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [0, 0, 1], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [1, 1, 0], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [1, 0, 1], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [0, 1, 1], axis=0)
+        u = np.append(u, np.transpose([x, y, z]) - [1, 1, 1], axis=0)
+        vertices[i] = np.unique(u, axis=0)
+        phases[i]   = phase[0]
+
+    ## Return the preprocessed data
+    return RVE, vertices, phases
