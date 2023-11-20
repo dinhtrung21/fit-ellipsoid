7590404141fef48bccb34c6a9781731220e9e675
Nguyen Trung <trung.nguyen@vtt.fi>
2023-11-20 23:24:30 +0200

    Update ellipsoid fitting script

--- fitellipsoid/ellipsoid.py
+++ fitellipsoid/ellipsoid.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-def fitEllipsoid(id):
+def fitEllipsoid(id, RVE, vertices):
     """
-        Returns the best-fitted ellipsoid to a 3D region defined by a set of points on xyz-coordinate.
+        Returns the best-fitted ellipsoid to a 3D region defined by a set of voxels' vertices on xyz-coordinate.
         Input:
-            id   : our grain ID
+            id       : our grain ID
+            RVE      : RVE dictionary from fitellipsoid/preprocessing
+            vertices : vertices dictionary from fitellipsoid/preprocessing
         Output
             mu_x, mu_y, mu_z: the center of our ellipsoid
             Sigma           : a matrix such that inv(Sigma) define our quadric surface
@@ -12,8 +14,9 @@ def fitEllipsoid(id):
             R               : the rotational matrix
     """
     ## Get the grain's vertices and volume
-    A = np.transpose(vertices[i])
-    n = np.shape(RVE[i])[0]
+    A = np.transpose(vertices[id])
+    n = np.shape(RVE[id])[0]
+
     ## Calculate the center of our ellipsoid
     mu_x = np.average(A[0])
     mu_y = np.average(A[1])
@@ -24,6 +27,7 @@ def fitEllipsoid(id):
              [np.cov(A[2],A[0])[0][1], np.cov(A[2],A[1])[0][1], np.cov(A[2],A[2])[0][1]]]
     ## Extract the axes and the rotation matrix
     L, R = np.linalg.eig(Sigma)
+
     ## Scale the axes by volume
     scaling = np.cbrt(3*n/(4*np.pi*np.sqrt(np.prod(L))))
     L = np.sqrt(L) * scaling
