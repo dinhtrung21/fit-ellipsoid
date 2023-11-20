6269d8ae93f1b5436c8f3f30234195d9996788e2
Nguyen Trung <trung.nguyen@vtt.fi>
2023-11-20 19:30:56 +0200

    Update preprocessing script

--- fitellipsoid/preprocessing.py
+++ fitellipsoid/preprocessing.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+def grains(data):
+    """
+        Generate a batch of images randomly sampled from a training microstructure
+        :param data: data path
+        :return: 3 dictionaries of RVE coordinates, voxels' vertices and voxels' phases
+    """
+    return data
