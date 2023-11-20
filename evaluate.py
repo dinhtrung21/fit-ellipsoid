1ef5b91e76e1d7f0a06316e67943595c6da9a162
Nguyen Trung <trung.nguyen@vtt.fi>
2023-11-21 00:54:39 +0200

    Update RVE constants

--- evaluate.py
+++ evaluate.py
@@ -2,7 +2,12 @@ from fitellipsoid import ellipsoid, util
 import numpy as np
 
 ## Input our parameters for size/shape distribution of each phases
-mu    = [1, 1, 1, 1]
-sigma = [1, 1, 1, 1]
-alpha = [1, 1, 1, 1]
-beta  = [1, 1, 1, 1]
+mu       = [1, 1, 1, 1]
+sigma    = [1, 1, 1, 1]
+alpha    = [1, 1, 1, 1]
+beta     = [1, 1, 1, 1]
+fraction = [1, 1, 1, 1]
+
+## RVE constants, including dimensions and resolution
+dim = 100
+res = 0.2
