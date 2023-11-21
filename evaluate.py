256b914279ac9fd5a6f4cb9b07107dc80fc3b22f
Nguyen Trung <trung.nguyen@vtt.fi>
2023-11-21 09:20:34 +0200

    Update evaluate script

--- evaluate.py
+++ evaluate.py
@@ -11,3 +11,11 @@ fraction = [1, 1, 1, 1]
 ## RVE constants, including dimensions and resolution
 dim = 100
 res = 0.2
+
+## Number of RVEs for evaluation
+n = 5
+
+## Loop through each RVE
+for i in range(1, n + 1):
+    data_path = f'data/{i}/QP_FFT_data'
+    d, a = ellipsoid.fitRVE(data_path)
