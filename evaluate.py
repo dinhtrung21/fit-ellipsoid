0b6552b5a4b1c3918a6bd4e3830b107fa33554bd
Trung <trung.t.nguyen@aalto.fi>
2023-11-29 09:26:49 +0200

    Update Hellinger error: not include phase fraction

--- evaluate.py
+++ evaluate.py
@@ -37,10 +37,10 @@ def RVE_difference(d, a):
     for i in d:
         ## Calculate the Hellinger distance of the size distribution
         mu_, sigma_ = util.fit_lognorm(d[i])
-        H_d        += fraction[i-1] * util.hellinger_lognorm(mu_, mu[i-1], sigma_, sigma[i-1])/np.sum(fraction)
+        H_d        += util.hellinger_lognorm(mu_, mu[i-1], sigma_, sigma[i-1])/4
         ## Calculate the Hellinger distance of the shape distribution
         ap_, be_ = util.fit_beta(a[i])
-        H_a     += fraction[i-1] * util.hellinger_beta(ap_, alpha[i-1], be_, beta[i-1])/np.sum(fraction)
+        H_a     += util.hellinger_beta(ap_, alpha[i-1], be_, beta[i-1])/4
     ## Calculate the difference
     E = (H_d + H_a)/2
     ## Return the difference
